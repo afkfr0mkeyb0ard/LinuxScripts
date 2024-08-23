af483d426e69483a22aa1af5ebc862f265888b8c
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-08-23 11:12:20 +0200

    Create resolver.sh

---
+++ resolver.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Resolve a list of domains given in a file (input) and resolve their IP in output file. 
+# It uses the host command
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+input_file="$1"
+output_file="$2"
+
+> "$output_file"
+
+while IFS= read -r domain
+do
+    host "$domain" | awk '/has address/ { print $4 }' >> "$output_file"
+done < "$input_file"
+
+echo "Domain resolution completed. IP addresses are in $output_file."
