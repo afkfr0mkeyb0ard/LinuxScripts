51cd7f8d1a4b1d34d49605049dc305f042236b8b
afkfr0mkeyb0ard <123593001+afkfr0mkeyb0ard@users.noreply.github.com>
2024-08-23 11:13:27 +0200

    Update resolver.sh

--- resolver.sh
+++ resolver.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Resolve a list of domains given in a file (input) and resolve their IP in output file. 
 # It uses the host command
+# Example: sh resolver.sh domains.txt resolved.txt
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <input_file> <output_file>"
